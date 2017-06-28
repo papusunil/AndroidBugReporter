e246699f128c95411c719672d5788c7e0bb191e0
Sunil Kumar <sunilkumar@Sunils-MacBook-Pro.local>
2017-06-27 22:45:15 -0500

    Floating reporter activity

---
+++ BugReporter/app/src/main/java/papusunil/com/bugreporter/FloatingReporter.java
@@ -0,0 +1,13 @@
+package papusunil.com.bugreporter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FloatingReporter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_floating_reporter);
+    }
+}
